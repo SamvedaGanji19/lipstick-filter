685b53f7395da1b1f9fb006d925d1a013edd47ed
SamvedaGanji19 <78354707+SamvedaGanji19@users.noreply.github.com>
2021-09-08 17:25:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX=0;
+noseY=0;
+
+
+function preload(){
+lipstick=loadImage('https://i.postimg.cc/1z28QMPB/s-l640-removebg-preview.png')
+}
+function setup(){
+   canvas=createCanvas(300,300);
+   canvas.center();
+   video=createCapture(VIDEO);
+   video.size(300,300);
+   video.hide();
+
+   poseNet=ml5.poseNet(video,modelLoaded);
+   poseNet.on ('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log('poseNet is Initialized');
+}
+function gotPoses(results){
+    if(results.length>0){ 
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nose x = "+ noseX);
+        console.log("nose y = "+ noseY);
+    }
+}
+function draw(){
+image(video,0,0,300,300);
+image(lipstick,noseX,noseY,30,30);
+}
+function take_snapshot(){
+    save('My_Filter_Image.png');
+}
